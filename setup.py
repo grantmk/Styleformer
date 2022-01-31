c487ab26de600ea99291ab95cb8306defac77f48
Grant Klimaytys <grantmk@users.noreply.github.com>
2022-01-31 12:10:16 +0000

    Remove sentencepiece dependancy as not used and not resolving with PIP

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="A Neural Language Style Transfer framework to transfer natural language text smoothly between fine-grained language styles like formal/casual, active/passive, and many more. Created by Prithiviraj Damodaran. Open to pull requests and other forms of collaboration.",
     url="https://github.com/PrithivirajDamodaran/Styleformer.git",
     packages=setuptools.find_packages(),
-    install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy'],
+    install_requires=['transformers', 'python-Levenshtein', 'fuzzywuzzy'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: Apache 2.0",
